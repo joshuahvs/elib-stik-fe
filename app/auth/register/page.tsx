281e8aa39455eb5e15bab2cc78be4d9724a2d271
Joshua Hans Vito Soehendra <thebestvitodiaries@gmail.com>
2026-04-19 23:09:14 +0700

    fix double pass register

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -242,16 +242,6 @@ export default function RegisterPage() {
                   />
                 )}
 
-                <InputField
-                  label="Kata Sandi *"
-                  labelClassName="text-white/80"
-                  name="password"
-                  type="password"
-                  placeholder="Kata Sandi"
-                  onChange={handleChange}
-                  value={form.password}
-                  required
-                />
                 <div className="relative">
                   <InputField
                     label="Kata Sandi *"
